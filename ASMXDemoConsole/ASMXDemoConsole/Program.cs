2440e30af39e89f6f8d396550091a92f23f592c1
manishkumar-agarwal <manish.kumar.agarwal1@gmail.com>
2016-03-25 15:28:42 +0530

    Demo for Console and Web Service

---
+++ ASMXDemoConsole/ASMXDemoConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASMXDemoConsole.ASMXDemoWebServiceReference;
+
+namespace ASMXDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ASMXDemoWebService asmxDemoService = new ASMXDemoWebService();
+            Console.WriteLine(asmxDemoService.HelloWorld()); 
+        }
+    }
+}
